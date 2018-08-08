8ac755ee284bcff277bfe47ea6e899870c91f491
Lopez66 <1120054743@qq.com>
2018-08-08 14:30:25 +0800

    使得可以正常导入

--- app/src/main/java/com/lopez/toast/MainActivity.java
+++ app/src/main/java/com/lopez/toast/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //也可以在Application中初始化
         /*ToastUtils toast = ToastUtils.initToast(this);
         toast.setLayout(R.layout.toast_layout_white);*/
     }
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     public void hello(View view) {
         //ToastUtils.showToast("你好啊！");
         //ToastUtils.showToast(R.string.hi, 1000);
-        ToastUtils.showToastLong("我很菜呀~");
+        //ToastUtils.showToastLong("我很菜呀~");
     }
 
 }
