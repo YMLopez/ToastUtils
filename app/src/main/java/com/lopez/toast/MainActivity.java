3ba0dde1571a0f660b8921adaacae27621abd002
Lopez66 <1120054743@qq.com>
2018-08-08 14:09:01 +0800

    去掉与版本有关引起的导入异常的bug

--- app/src/main/java/com/lopez/toast/MainActivity.java
+++ app/src/main/java/com/lopez/toast/MainActivity.java
@@ -1,13 +1,13 @@
 package com.lopez.toast;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.lopez.toast.utils.ToastUtils;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
